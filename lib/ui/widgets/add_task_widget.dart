cb5d65dc727f30f222e4af5ead4d71c6b6f003e2
Abhay <abhay.kachhwaha09@gmail.com>
2024-02-02 13:36:02 +0530

    Resuable task widget

---
+++ lib/ui/widgets/add_task_widget.dart
@@ -0,0 +1,66 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_app/services/firestore_database.dart';
+import 'package:todo_app/ui/utilities/validator.dart';
+
+import '../../models/task_model.dart';
+
+class AddTaskWidget extends StatefulWidget {
+  const AddTaskWidget({super.key});
+
+  @override
+  State<AddTaskWidget> createState() => _AddTaskWidgetState();
+}
+
+class _AddTaskWidgetState extends State<AddTaskWidget> {
+  final _formKey = GlobalKey<FormState>();
+  final _descriptionTextController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      scrollable: true,
+      title: const Text('Add Task'),
+      content: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _descriptionTextController,
+                validator: (value) => Validator.validateDescription(value),
+                decoration: const InputDecoration(
+                  labelText: 'Description',
+                  icon: Icon(Icons.edit),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      actions: [
+        ElevatedButton(
+          child: const Text("Add"),
+          onPressed: () => addTask(
+            _descriptionTextController.text,
+          ),
+        ),
+      ],
+    );
+  }
+
+  void addTask(
+    String description,
+  ) {
+    final isValid = _formKey.currentState!.validate();
+
+    if (isValid) {
+      FirestoreDatabase.addTask(
+        FirebaseAuth.instance.currentUser!.uid,
+        Task(description: description),
+      );
+      Navigator.of(context).pop();
+    }
+  }
+}
---
+++ lib/ui/widgets/edit_task_widget.dart
@@ -0,0 +1,81 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../../models/task_model.dart';
+import '../../services/firestore_database.dart';
+import '../utilities/validator.dart';
+
+class EditTaskWidget extends StatefulWidget {
+  const EditTaskWidget({super.key, required this.task});
+  final Task task;
+
+  @override
+  State<EditTaskWidget> createState() => _EditTaskWidgetState();
+}
+
+class _EditTaskWidgetState extends State<EditTaskWidget> {
+  final _formKey = GlobalKey<FormState>();
+  final _descriptionTextController = TextEditingController();
+  String? _oldDescription;
+
+  @override
+  void initState() {
+    _oldDescription = widget.task.description;
+    _descriptionTextController.text = widget.task.description;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      scrollable: true,
+      title: const Text('Edit Task'),
+      content: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _descriptionTextController,
+                validator: (value) => Validator.validateDescription(value),
+                decoration: const InputDecoration(
+                  labelText: 'Description',
+                  icon: Icon(Icons.edit),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      actions: [
+        ElevatedButton(
+          child: const Text("Edit"),
+          onPressed: () =>
+              editTask(_descriptionTextController.text, _oldDescription),
+        ),
+      ],
+    );
+  }
+
+  void editTask(
+    String description,
+    String? oldDescription,
+  ) {
+    final isValid = _formKey.currentState!.validate();
+
+    if (isValid) {
+      // FirestoreDatabase.removeTask(
+      //   FirebaseAuth.instance.currentUser!.uid,
+      //   Task(
+      //     description: oldDescription!,
+      //     isCompleted: widget.task.isCompleted,
+      //   ),
+      // );
+      FirestoreDatabase.editTask(FirebaseAuth.instance.currentUser!.uid,
+          Task(description: description), oldDescription);
+      Navigator.of(context).pop();
+    }
+  }
+}
